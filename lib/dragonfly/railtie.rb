bfab22894f1e6dd476945e0a7718d9f962532ea6
Dave Rapin <dave@rapin.com>
2012-11-22 18:19:04 -0500

    commented out the dragonfly cookie middleware temporarily to avoid rails-api issues"

--- lib/dragonfly/railtie.rb
+++ lib/dragonfly/railtie.rb
@@ -4,7 +4,8 @@ require 'rails'
 module Dragonfly
   class Railtie < ::Rails::Railtie
     initializer "dragonfly.railtie.initializer" do |app|
-      app.middleware.insert_before 'ActionDispatch::Cookies', Dragonfly::CookieMonster
+      # Commenting out temporarily to avoid errors when using rails-api gem
+      #app.middleware.insert_before 'ActionDispatch::Cookies', Dragonfly::CookieMonster
     end
   end
 end
